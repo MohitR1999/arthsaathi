8858fced11f8533c885e53cec8895ff3e7fd6491
MohitR1999 <mohitr1999@gmail.com>
2025-09-22 20:19:39 +0530

    Added Database interface against which all DB operations shall be written

---
+++ as-auth/db/Database.js
@@ -0,0 +1,27 @@
+class Database {
+    constructor(options) {
+        this.options = options;
+    }
+
+    create = async () => {
+
+    }
+
+    getById = async () => {
+
+    }
+
+    getAll = async () => {
+
+    }
+
+    update = async () => {
+
+    }
+
+    remove = async () => {
+
+    }
+}
+
+module.exports = Database;
