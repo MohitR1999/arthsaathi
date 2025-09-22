2b6628fab947e36699e65fb94a12325fd9babe86
MohitR1999 <mohitr1999@gmail.com>
2025-09-22 20:49:00 +0530

    added complete testcases for mock db

--- as-auth/db/MockDatabase.js
+++ as-auth/db/MockDatabase.js
@@ -1,31 +1,49 @@
 const Database = require("./Database");
+const User = require("../models/User");
 
 class MockDatabase extends Database {
     constructor(options) {
         super(options);
         this.options = options;
         this.database = [];
+        this.database.push(new User({
+            firstName: "Test",
+            lastName: "Test",
+            email: "test@xyz.com",
+            password: "test456",
+            id: "5678"
+        }))
     }
 
     create = async (user) => {
-        this.database.push(user);    
+        this.database.push(user);
         return true;
     }
 
-    getById = async () => {
-
+    getById = async (id) => {
+        const user = this.database.find(user => user.id == id);
+        return user;
     }
 
     getAll = async () => {
-
+        return this.database;
     }
 
-    update = async () => {
-
+    update = async (user) => {
+        for (let i = 0; i < this.database.length; i++) {
+            if (this.database[i].id == user.id) {
+                this.database[i] = user;
+                return true;
+            }
+        }
     }
 
-    remove = async () => {
-
+    remove = async (id) => {
+        const index = this.database.findIndex(user => user.id == id);
+        if (index > -1) {
+            this.database.splice(index, 1);
+            return true;
+        }
     }
 }
 
