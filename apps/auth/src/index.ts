158c1d8ad8b4b81a13308083a667c82838a3a3c0
MohitR1999 <mohitr1999@gmail.com>
2025-11-02 17:08:15 +0530

    added basic code for running app

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -1,7 +1,34 @@
-import { app } from './app/app'
+import { makeApp } from './app';
+import { Sequelize } from 'sequelize';
 
-const PORT = process.env.PORT || 5000
+const database = process.env.MYSQL_DATABASE;
+const username = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOSTNAME;
+const dialect = 'mysql';
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}!`)
-})
+const sequelize = new Sequelize(database ?? 'authdb', username ?? 'authuser', password, {
+    host,
+    dialect
+});
+
+const runApp = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to database established successfully!');
+        const app = makeApp(sequelize);
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}!`);
+        });
+        process.on('SIGTERM', () => {
+            console.log('Shutting down server!!');
+            sequelize.close();
+        });
+    } catch (error) {
+        console.log('Some error occured! App will not start!!!');
+        console.log(error);
+    }
+};
+
+runApp();
