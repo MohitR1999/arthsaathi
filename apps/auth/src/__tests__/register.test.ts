c9e8b90a317c8a7b43d9bdc99288ff0468449def
MohitR1999 <mohitr1999@gmail.com>
2025-11-02 20:58:01 +0530

    added all testcases for register endpoint

--- apps/auth/src/__tests__/register.test.ts
+++ apps/auth/src/__tests__/register.test.ts
@@ -6,7 +6,7 @@ import { Sequelize } from 'sequelize';
 import { MESSAGES } from "@arthsaathi/helpers/messages";
 import { STATUS_CODES } from "@arthsaathi/helpers/codes";
 
-describe('Register endpoint test', () => {
+describe('Register endpoint test - Successful scenarios', () => {
     let app: Express;
     let sequelize: Sequelize;
 
@@ -27,4 +27,97 @@ describe('Register endpoint test', () => {
         expect(res.status).toBe(STATUS_CODES.CREATED);
         expect(res.body.message).toBe(MESSAGES.SUCCESSFUL_REGISTER);
     });
+});
+
+describe("Register endpoint test - Unsuccessful scenarios", () => {
+    let app: Express;
+    let sequelize: Sequelize;
+
+    beforeAll(async () => {
+        sequelize = new Sequelize();
+        app = await makeApp(sequelize);
+    });
+    
+    const userToBeRegisterdWithInvalidFirstName = {
+        firstName : "Mohit<",
+        lastName : "Ranjan",
+        email : "a@b.com",
+        password : "testpassword"
+    };
+    
+    const userToBeRegisterdWithInvalidLastName = {
+        firstName : "Mohit",
+        lastName : ":>",
+        email : "test@pqr.com",
+        password : "testpassword"
+    };
+    
+    const userToBeRegisterdWithInvalidEmail = {
+        firstName : "Mohit",
+        lastName : "Ranjan",
+        email : "testabcsdf.com",
+        password : "testpassword"
+    };
+    
+    const userToBeRegisterdWithInvalidPassword = {
+        firstName : "Mohit",
+        lastName : "Ranjan",
+        email : "test@www.com",
+        password : ""
+    };
+    
+    const userToBeRegisterd = {
+        firstName : "Mohit",
+        lastName : "Ranjan",
+        email : "test@ttt.com",
+        password : "testpassword"
+    };
+
+    const userToBeRegisterdThatThrowsException = {
+        firstName : "Mohit",
+        lastName : "Ranjan",
+        email : "test@throwsexception.com",
+        password : "password"
+    }
+
+    it("should throw error in case first name is not valid", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterdWithInvalidFirstName});
+        expect(res.status).toEqual(STATUS_CODES.BAD_REQUEST);
+        expect(res.body.message).toEqual(MESSAGES.INVALID_FIRST_NAME);
+    });
+
+    it("should throw error in case last name is not valid", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterdWithInvalidLastName});
+        expect(res.status).toEqual(STATUS_CODES.BAD_REQUEST);
+        expect(res.body.message).toEqual(MESSAGES.INVALID_LAST_NAME);
+    });
+
+    it("should throw error in case email is not valid", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterdWithInvalidEmail});
+        expect(res.status).toEqual(STATUS_CODES.BAD_REQUEST);
+        expect(res.body.message).toEqual(MESSAGES.INVALID_EMAIL);
+    });
+    
+    it("should throw error in case password is not valid", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterdWithInvalidPassword});
+        expect(res.status).toEqual(STATUS_CODES.BAD_REQUEST);
+        expect(res.body.message).toEqual(MESSAGES.INVALID_PASSWORD);
+    });
+    
+    it("should throw error in case client tries to register with same email ID", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterd});
+        expect(res.status).toBe(STATUS_CODES.CREATED);
+        expect(res.body.message).toBe(MESSAGES.SUCCESSFUL_REGISTER);
+
+        const errorRes = await supertest(app).post("/api/auth/register").send({...userToBeRegisterd});
+        expect(errorRes.status).toEqual(STATUS_CODES.BAD_REQUEST);
+        expect(errorRes.body.message).toEqual(MESSAGES.DUPLICATE_EMAIL);
+    });
+
+    it("should throw internal server error in case any other error occurs", async () => {
+        const res = await supertest(app).post("/api/auth/register").send({...userToBeRegisterdThatThrowsException});
+        expect(res.status).toEqual(STATUS_CODES.SERVER_ERROR);
+        expect(res.body.message).toEqual(MESSAGES.SERVER_ERROR);
+    });
+
 });
