887602bc5e8636d2a260e51b4fcab2703803e317
MohitR1999 <mohitr1999@gmail.com>
2025-11-05 22:53:32 +0530

    added database initialization logic with credentials

--- apps/money-handler/src/index.ts
+++ apps/money-handler/src/index.ts
@@ -1,10 +1,22 @@
 import { makeApp } from "./app";
+import { Sequelize } from 'sequelize';
+
+const database = process.env.MYSQL_DATABASE;
+const username = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const host = process.env.MYSQL_HOSTNAME;
+const dialect = 'mysql';
 
 const PORT = process.env.PORT || 5001;
 
+const sequelize = new Sequelize(database ?? 'moneydb', username ?? 'moneyuser', password, {
+    host,
+    dialect
+});
+
 const runApp = async () => {
     try {
-        const app = await makeApp();
+        const app = await makeApp(sequelize);
         app.listen(PORT, () => {
             console.log(`Money handler app running on port: ${PORT}`);
         });
