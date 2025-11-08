28e851dda12a613fca754f1ce4350edc8896e808
MohitR1999 <mohitr1999@gmail.com>
2025-11-08 14:25:57 +0530

    added route exports for put and delete

--- apps/money-handler/src/handlers/index.ts
+++ apps/money-handler/src/handlers/index.ts
@@ -1,2 +1,4 @@
 export { makeHandler as makeCategoryPostHandler } from "./category/post";
 export { makeHandler as makeCategoryGetHandler } from "./category/get";
+export { makeHandler as makeCategoryDeleteHandler } from "./category/delete";
+export { makeHandler as makeCategoryPutHandler } from "./category/put";
