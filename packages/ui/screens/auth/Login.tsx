ca1a913f522b0f9ea9a92bec9d80bbc9584a80f0
MohitR1999 <mohitr1999@gmail.com>
2025-11-16 20:50:50 +0530

    added basic packages for start off

---
+++ packages/ui/screens/auth/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeLayout } from "../../components/SafeLayout";
+import { Text } from "react-native-paper";
+
+const Login = () => {
+  return (
+    <SafeLayout>
+      <Text>Login page</Text>
+    </SafeLayout>
+  );
+};
+
+export { Login };
---
+++ packages/ui/screens/auth/Register.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeLayout } from "../../components/SafeLayout";
+import { Text } from "react-native-paper";
+
+const Register = () => {
+  return (
+    <SafeLayout>
+      <Text>Register page</Text>
+    </SafeLayout>
+  );
+};
+
+export { Register };
