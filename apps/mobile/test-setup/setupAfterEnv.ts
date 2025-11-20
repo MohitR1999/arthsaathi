4986a0d533973279f9e10726f8b017291ceea7c0
MohitR1999 <mohitr1999@gmail.com>
2025-11-20 19:55:59 +0530

    added msw server for running in tests

--- apps/mobile/test-setup/setupAfterEnv.ts
+++ apps/mobile/test-setup/setupAfterEnv.ts
@@ -1,1 +1,15 @@
 import './mocks/msw.polyfills'
+
+import { server } from './mocks/server'
+
+beforeAll(() => {
+    server.listen()
+})
+
+afterEach(() => {
+    server.resetHandlers()
+})
+
+afterAll(() => {
+    server.close()
+})
