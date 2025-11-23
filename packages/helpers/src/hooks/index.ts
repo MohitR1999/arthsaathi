6c27fedc4ebbfea21981a03e5fafd6a16cbad4eb
Mohit Ranjan <mohitr1999@gmail.com>
2025-11-23 16:34:43 +0530

    added query for fetching user profile in profile page (#19)

--- packages/helpers/src/hooks/index.ts
+++ packages/helpers/src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useAuthStore } from './useAuthStore'
 export { useLogin } from './auth/useLogin'
+export { useProfile } from './profile/useProfile'
--- packages/ui/screens/profile/Profile.tsx
+++ packages/ui/screens/profile/Profile.tsx
@@ -1,48 +1,65 @@
 import React from "react";
 import { SafeLayout } from "../../components";
-import { Text, List, useTheme, Divider } from "react-native-paper";
+import {
+  Text,
+  List,
+  useTheme,
+  Divider,
+  ActivityIndicator,
+} from "react-native-paper";
 import { View } from "react-native";
 
 type ProfileScreenProps = {
   logout: () => void;
+  firstName: string;
+  loading: boolean;
 };
 
-const Profile = ({ logout }: ProfileScreenProps) => {
+const Profile = ({ logout, firstName, loading }: ProfileScreenProps) => {
   const theme = useTheme();
   return (
     <SafeLayout>
-      <View
-        style={{
-          paddingHorizontal: 12,
-          paddingTop: 12,
-        }}
-      >
-        <Text variant="headlineMedium">Welcome</Text>
-      </View>
-      <List.Section>
-        <List.Subheader>
-          <Text variant="titleMedium">Security</Text>
-        </List.Subheader>
-        <Divider />
-        <View
-          style={{
-            paddingLeft: 12,
-          }}
-        >
-          <List.Item
-            title="Logout"
-            onPress={() => {
-              logout();
+      {loading ? (
+        <ActivityIndicator size={'large'} />
+      ) : (
+        <>
+          <View
+            style={{
+              paddingHorizontal: 12,
+              paddingTop: 12,
             }}
-            left={() => (
-              <List.Icon color={theme.colors.secondary} icon="logout" />
-            )}
-            right={() => (
-              <List.Icon color={theme.colors.secondary} icon="chevron-right" />
-            )}
-          />
-        </View>
-      </List.Section>
+          >
+            <Text variant="headlineMedium">{`Welcome ${firstName}`}</Text>
+          </View>
+          <List.Section>
+            <List.Subheader>
+              <Text variant="titleMedium">Security</Text>
+            </List.Subheader>
+            <Divider />
+            <View
+              style={{
+                paddingLeft: 12,
+              }}
+            >
+              <List.Item
+                title="Logout"
+                onPress={() => {
+                  logout();
+                }}
+                left={() => (
+                  <List.Icon color={theme.colors.secondary} icon="logout" />
+                )}
+                right={() => (
+                  <List.Icon
+                    color={theme.colors.secondary}
+                    icon="chevron-right"
+                  />
+                )}
+              />
+            </View>
+          </List.Section>
+        </>
+      )}
     </SafeLayout>
   );
 };
